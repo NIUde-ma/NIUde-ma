fadea82328e2b30f27d162ae85f57201ff76001e
NIU-ma <maf629209@gmail.com>
2024-11-28 02:13:13 +0000

    add auto get cookies

--- good_play/xinfan.py
+++ good_play/xinfan.py
@@ -3,9 +3,49 @@
 
 import requests
 import time
-from fake_useragent import UserAgent
-from datetime import datetime
 import json
+from datetime import datetime
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+
+# def get_cookie(login_urls):
+#     ua = UserAgent()
+    
+#     headers = {
+#         "User-Agent": ua.random,
+#         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#         "Accept-Encoding": "gzip, deflate, br, zstd",
+#         "Accept-Language":"en-US,en;q=0.5",
+#         # "User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:132.0) Gecko/20100101 Firefox/132.0",
+#         "Connection": "keep-alive",
+#         "Cookie":"SESSION=bd3b7c41-e39c-41fc-8aad-671c00358a6c"
+#     }
+
+#     session = requests.Session()
+#     # session.headers.update(headers)
+
+#     login_data = {
+#         "login": "wjjiang10",
+#         "password": "Jieshao520"
+#     }
+    
+#     # response = session.get(login_urls, headers=headers, data=login_data, allow_redirects=False)
+    
+
+#     # if response.status_code != 200:
+#     #     print("Login failed",response.status_code)
+#     #     return None
+    
+#     redirect_url = "https://ipark.iflytek.com/portal-web/pc/menu/food/weekMenu"
+#     final_response = session.get(redirect_url,headers=headers, data=login_data, allow_redirects=False)
+#     print(final_response.text)
+#     cookies = session.cookies.get_dict()
+    
+#     idd = cookies.get('JSESSIONID')
+#     print(session.cookies)
+#     print(idd)
+#     return idd
 
 def get_json_():
     
@@ -19,7 +59,8 @@ def get_json_():
         fan = data.get("kunkun_fan", "Unknown")
         kunming = data.get("kunkun_jiming", "Unknown")
         Cookies = data.get("Cookie","Unknown")
-        return a2_url_local_am, a2_url_local_pm, a4_url_local_am, a4_url_local_pm, yuanshen, fan, kunming, Cookies
+        login_urls = data.get("test_url","Unknown")
+        return a2_url_local_am, a2_url_local_pm, a4_url_local_am, a4_url_local_pm, yuanshen, fan, kunming, Cookies, login_urls
 
 def get_local():
     currentDateAndTime = datetime.now()
@@ -29,6 +70,8 @@ def get_local():
     return times,month_and_day
 
 def get_Food_a2(times,month_and_day,a2_url_local_am,a2_url_local_pm, Cookies):
+    # passwds = "JSESSIONID=" + idd
+    # print(passwds)
     if times <= 12:
         date = "AM"
         a2_url = a2_url_local_am
@@ -105,6 +148,8 @@ def get_Food_a2(times,month_and_day,a2_url_local_am,a2_url_local_pm, Cookies):
     return message_a2
 
 def get_Food_a4(times,month_and_day,a4_url_local_am, a4_url_local_pm, Cookies):
+    # passwds = "JSESSIONID=" + idd
+
     if times <= 12:
         date = "AM"
         a4_url = a4_url_local_am
@@ -155,7 +200,7 @@ def get_Food_a4(times,month_and_day,a4_url_local_am, a4_url_local_pm, Cookies):
     
 
 
-def send_kunkun(food, kunming):
+def send_kunkun(food, fan):
     send_url = kunming
     response_a2 = requests.post(send_url, json=food_a2)
     
@@ -165,7 +210,7 @@ def send_kunkun(food, kunming):
     else:
         print("请求失败:", response_a2.status_code, response_a2.text)
 
-def send_kunkun2(food, kunming):
+def send_kunkun2(food, fan):
     send_url = kunming
     response_a4 = requests.post(send_url, json=food_a4)
     
@@ -177,9 +222,11 @@ def send_kunkun2(food, kunming):
 
 if __name__ == '__main__':
     times , month_and_day = get_local()
-    a2_url_local_am, a2_url_local_pm, a4_url_local_am, a4_url_local_pm, yuanshen, fan, kunming, Cookies = get_json_()
+    a2_url_local_am, a2_url_local_pm, a4_url_local_am, a4_url_local_pm, yuanshen, fan, kunming, Cookies, login_urls = get_json_()
+    # idd = get_cookie(login_urls)
     food_a2 = get_Food_a2(times, month_and_day,a2_url_local_am, a2_url_local_pm, Cookies)
     food_a4 = get_Food_a4(times, month_and_day,a4_url_local_am, a4_url_local_pm, Cookies)
-    send_kunkun(food_a2,kunming)
+    send_kunkun(food_a2,fan)
     time.sleep(3)
-    send_kunkun2(food_a4,kunming)
+    send_kunkun2(food_a4,fan)
+    # get_cookie(login_urls)
